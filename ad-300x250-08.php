5e1cad232a26ff029b80209881a9727b1d271dee
Doron Orenstein <jazziseverywhere@gmail.com>
2018-06-12 06:15:59 -0500

    added new sidebar 300x250 slot

---
+++ ad-300x250-08.php
@@ -0,0 +1,29 @@
+<?php
+
+// AD SLOT 10 & 11
+
+$Img1 = get_bloginfo('url') . "/wp-content/uploads/whisper-room-1.jpg";
+$Alt1 = "Whisper Room";
+$Url1 = "https://www.whisperroom.com";
+$ad_name1 = "Whisper-Room-Practice-Without-Disturbing-Your-Neighbors";
+$slot_name1 = "Whisper Room - Practice Without Disturbing Your Neighbors";
+
+$Img2 = get_bloginfo('url') . "/wp-content/uploads/whisper-room-1.jpg";
+$Alt2 = "Whisper Room";
+$Url2 = "https://www.whisperroom.com";
+$ad_name2 = "Whisper-Room-Practice-Without-Disturbing-Your-Neighbors";
+$slot_name2 = "Whisper Room - Practice Without Disturbing Your Neighbors";
+
+
+$num = mt_rand(1,2);
+
+$Image = ${'Img'.$num};
+$Alt = ${'Alt' .$num};
+$URL = ${'Url'.$num};
+$ad_name = ${'ad_name'.$num};
+$slot_name = ${'slot_name'.$num};
+
+print "<div class=\"sidebar-ads ad\" data-ad-slot=\"ad-300x250-07\"><a href=\"".$URL."\" target=\"_blank\"
+class=\"ad-banner\" data-ad-name=\"".$ad_name."\" data-slot-name=\"".$slot_name."\"><img src=\"".$Image."\" alt=\"".$Alt."\" width=\"300\"
+height=\"250\"></a></div>";
+?>
